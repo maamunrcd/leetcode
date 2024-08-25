346d3c824ba6e683adcb49dcfd7b0c04d3592cf6
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2024-08-25 15:51:58 +0600

    Time: 80 ms (29.67%), Space: 58.9 MB (71.30%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let profit = 0;
+    let left = 0;
+    let right = left + 1;
+    while(left < right && right < prices.length){
+        profit = Math.max(profit, (prices[right] - prices[left]));
+        if(prices[left] > prices[right]){
+            left++;
+            right = left+1;
+        } else {
+            right++;
+        }
+    }
+    return profit;
+};

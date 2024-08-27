cfa2e3cecc8d2f9788ec06f31b2b2de316b50a9a
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2024-08-27 11:55:16 +0600

    Time: 345 ms (5.13%), Space: 56.3 MB (5.13%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,21 +4,20 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    for(let i = 0; i < nums1.length; i++){
-        for(let j = 0; j < nums2.length; j++){
-            if(nums1[i] == nums2[j]){
-                for(let k = j; k < nums2.length; k++){
-                    if(nums2[k] > nums1[i]){
-                        nums1[i] = nums2[k];
-                        break;
-                    }
-                    if(k === nums2.length-1){
-                        nums1[i] = -1;
-                    }
-                }
+    let nums1Index = nums1.reduce((acc, curr, index) => ({ ...acc, [curr]: index }), {});
+    let resArray = Array.from({ length: nums1.length }, () => -1);
+    
+    for(let i = 0; i < nums2.length; i++){
+        if(nums1Index[nums2[i]] === undefined) continue;
+        
+        for(let j = i + 1; j < nums2.length; j++){
+            if(nums2[j] > nums2[i]){
+                let id = nums1Index[nums2[i]];
+                resArray[id] = nums2[j];
                 break;
             }
         }
     }
-    return nums1;
+    
+    return resArray;
 };

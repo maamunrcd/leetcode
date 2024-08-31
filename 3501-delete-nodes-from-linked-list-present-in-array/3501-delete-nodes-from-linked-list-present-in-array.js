98457e881df1c974305c0b550ac95fab2884a1c1
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2024-08-31 09:17:58 +0600

    Time: 420 ms (7.74%), Space: 97.5 MB (16.74%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var modifiedList = function(nums, head) {
+    let dummy = new ListNode(-1);
+    dummy.next = head;
+    let prev = dummy;
+    let current = head;
+    let set = new Set(nums);
+    while(current){
+        if(set.has(current.val)){
+            prev.next = current.next;
+            current = current.next;
+        } else {
+            prev = current;
+            current = current.next;
+        }
+    }
+    return dummy.next;
+};

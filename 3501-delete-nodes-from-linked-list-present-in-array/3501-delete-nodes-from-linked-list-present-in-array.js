21e141191bde3d7f4e160de9bec9fc2b61a81a73
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2024-08-31 09:20:11 +0600

    Time: 355 ms (69.62%), Space: 97.2 MB (19.55%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.js
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.js
@@ -15,7 +15,10 @@ var modifiedList = function(nums, head) {
     dummy.next = head;
     let prev = dummy;
     let current = head;
-    let set = new Set(nums);
+    let set = new Set();
+    for(item of nums){
+        set.add(item);
+    }
     while(current){
         if(set.has(current.val)){
             prev.next = current.next;

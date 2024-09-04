fab49cbbf248f44254388294aa312721602a9ac9
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2024-09-04 16:41:17 +0600

    Time: 56 ms (35.64%), Space: 49.5 MB (14.07%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -7,10 +7,12 @@ var strStr = function(haystack, needle) {
     for(let i = 0; i < haystack.length; i++){
         if(haystack[i] === needle[0]){
             for(let j = 0; j < needle.length; j++){
-                if(haystack[i + j] ===needle[j]){
+                if(haystack[i + j] === needle[j]){
                     if(j === needle.length - 1){
                         return i;
                     }
+                } else {
+                    break;
                 }
             }
         }

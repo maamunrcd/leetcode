3f8fdf64dbf6179857dabe3f766bf2b685e912b0
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2024-09-04 16:49:54 +0600

    Time: 53 ms (73.93%), Space: 50.3 MB (19.37%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -5,17 +5,16 @@
  */
 var twoSum = function(numbers, target) {
     let left = 0;
-    let right = numbers.length-1;
+    let right = numbers.length -1;
     while(left < right){
-        if((numbers[left]+ numbers[right]) == target){
+        let sum = numbers[left] + numbers[right];
+        if( sum == target){
             return [left+1, right+1];
         }
-        if(numbers[left]+ numbers[right] > target){
+        if(sum > target){
             right--;
         } else {
             left++;
         }
-        
     }
-    return [];
 };

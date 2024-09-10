5028c634f85d8b043587cb9e949f94d8f37805fb
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2024-09-10 15:08:59 +0600

    Time: 56 ms (82.39%), Space: 52.6 MB (53.38%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    const result = [];
+    const operators = ['+', '-', '*', '/'];
+
+    for(const char of tokens){
+        let op1, op2;
+        if(operators.includes(char)){
+            op2 = result.pop();
+            op1 = result.pop();
+        }
+        switch(char) {
+            case '+':
+             result.push(op1 + op2);
+             break;
+            case '-':
+              result.push(op1 - op2);
+              break;
+            case '*':
+              result.push(op1 * op2);
+              break;
+            case '/':
+              result.push(parseInt(op1 / op2));
+              break;
+            default:
+                result.push(+char);
+        }
+    }
+    return result;
+};

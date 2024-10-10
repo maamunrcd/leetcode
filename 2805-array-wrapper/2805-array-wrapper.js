020255306c58f11170b719e2206f672aea40b046
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2024-10-10 22:23:11 +0600

    Time: 62 ms (11.27%), Space: 51.1 MB (5.14%) - LeetHub

---
+++ 2805-array-wrapper/2805-array-wrapper.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.value = [...nums];
+};
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    return this.value.reduce((acc, val)=> acc + val, 0);
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return JSON.stringify(this.value);
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */

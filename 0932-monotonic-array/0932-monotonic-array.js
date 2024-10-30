7f657d54ccbde52159f2531fd6e2d8b5902be3da
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2024-10-30 15:59:44 +0600

    Time: 3 ms (50%), Space: 60.9 MB (73.15%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.js
+++ 0932-monotonic-array/0932-monotonic-array.js
@@ -3,13 +3,14 @@
  * @return {boolean}
  */
 var isMonotonic = function(nums) {
-    if((nums[nums.length -1] - nums[0]) < 0){
-        nums.reverse();
-    }
-    for(let i = 0; i < nums.length -1; i++){
+    let increase = true, decrease = true;
+    for(let i = 0; i < nums.length-1; i++){
         if(!(nums[i] <= nums[i+1])){
-            return false;
+            increase = false;
+        }
+        if(!(nums[i] >= nums[i+1])){
+            decrease = false;
         }
     }
-    return true;
+    return increase || decrease;
 };

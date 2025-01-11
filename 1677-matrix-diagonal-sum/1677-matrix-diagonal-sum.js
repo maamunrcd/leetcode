08561b1e5d515a4cc3359346f9c41d1a634d7734
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-01-11 20:19:02 +0600

    Time: 0 ms (100%), Space: 51.5 MB (13.17%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.js
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.js
@@ -6,6 +6,8 @@ var diagonalSum = function(mat) {
     let result = 0;
     for(let i = 0; i < mat.length; i++){
         result+= mat[i][i];
+    }
+    for(let i = 0; i < mat.length; i++){
         result+= mat[i][mat.length - 1 - i];
     }
     if (mat.length % 2 === 1) {

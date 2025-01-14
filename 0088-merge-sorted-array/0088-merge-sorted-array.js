4c9a16d3245bba096d13957a217dcee60311d255
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-01-14 08:23:37 +0600

    Time: 0 ms (100%), Space: 49.4 MB (30.07%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,7 +6,7 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let last = m + n -1;
+    let last = nums1.length -1;
     while(m > 0 && n > 0){
         if(nums1[m-1] > nums2[n-1]) {
             nums1[last] = nums1[m-1];

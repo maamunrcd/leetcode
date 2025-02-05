1c8f5382cd90ddb7ffc535ab5163a44814f61735
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-02-05 08:45:13 +0600

    Time: 1 ms (63.39%), Space: 51.1 MB (99.2%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -7,8 +7,8 @@ var removeDuplicates = function(nums) {
     let left = 1;
     for(let i = 1; i < nums.length; i++){
         if(nums[i] !== nums[i-1]){
-            nums[left] = nums[i]
-            left++;
+            nums[left] = nums[i];
+            left++; 
         }
     }
     return left;

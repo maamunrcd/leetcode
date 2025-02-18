629b3bf8dae11f8de77552e569c07faa6ceac10d
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-02-18 10:00:40 +0600

    Time: 2 ms (51.85%), Space: 55.6 MB (9.2%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ var dfs = function(root){
+    if(root === null) return [true, 0];
+    let left, right, balanced;
+    left = dfs(root.left);
+    right = dfs(root.right);
+    balanced = (left[0] && right[0] && (Math.abs(left[1] - right[1]) <=1));
+    return [balanced, 1 + Math.max(left[1], right[1])];
+ }
+var isBalanced = function(root) {
+    return dfs(root)[0];
+};

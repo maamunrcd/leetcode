0bfe64b60adeecc57d7401a0bb6f0ff38f350026
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-02-18 10:06:16 +0600

    Time: 1 ms (89.02%), Space: 56.4 MB (5.67%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -9,15 +9,15 @@
 /**
  * @param {TreeNode} root
  * @return {boolean}
- */
- var dfs = function(root){
+**/
+var dfs = function(root){
     if(root === null) return [true, 0];
     let left, right, balanced;
     left = dfs(root.left);
     right = dfs(root.right);
-    balanced = (left[0] && right[0] && (Math.abs(left[1] - right[1]) <=1));
+    balanced = left[0] && right[0] && (Math.abs(left[1] - right[1]) <=1);
     return [balanced, 1 + Math.max(left[1], right[1])];
- }
+}
 var isBalanced = function(root) {
     return dfs(root)[0];
 };

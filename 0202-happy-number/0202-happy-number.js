4735f309870e85bf85c5f7972f6912ee5dfd7fa1
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-02-21 16:29:19 +0600

    Time: 1 ms (81.33%), Space: 53.3 MB (9.95%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -2,15 +2,14 @@
  * @param {number} n
  * @return {boolean}
  */
-
 var sumOfSquares = function(n){
     let sum = 0;
-    while(n > 0){
+    while(n > 0) {
         let digit = n % 10;
         sum+= digit * digit;
         n = Math.floor(n / 10);
-     }
-     return sum;
+    }
+    return sum;
 }
 var isHappy = function(n) {
     let slow = n;
@@ -20,5 +19,4 @@ var isHappy = function(n) {
         fast = sumOfSquares(sumOfSquares(fast))
     } while(slow !== fast);
     return slow === 1;
-
 };

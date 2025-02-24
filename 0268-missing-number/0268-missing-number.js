3a9560fb0e732458811bbaadb314d3b1a895df0c
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-02-24 09:30:34 +0600

    Time: 0 ms (100%), Space: 54.9 MB (15.59%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,7 +3,9 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    const expectedSum = nums.length * (nums.length + 1) / 2;
-    const actualSum = nums.reduce((acc, item)=> acc + item, 0);
-    return expectedSum - actualSum;
+    let expectedSum = nums.length * (nums.length + 1) / 2;
+    for(let i = 0; i < nums.length; i++){
+        expectedSum-=nums[i];
+    }
+    return expectedSum;
 };

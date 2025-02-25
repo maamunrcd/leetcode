cb5ed521d23df492b908900398bc0e396d4ba385
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-02-25 10:28:47 +0600

    Time: 5 ms (93.18%), Space: 56.8 MB (29.73%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,18 +3,17 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    const objMap = new Map();
+    const newMap = new Map();
     let maxLength = 0;
     let left, right;
     left = 0;
     right = 0;
     while(right < s.length){
-        if(objMap.has(s[right]) && objMap.get(s[right]) >= left){
-            left = objMap.get(s[right]) + 1;
+        if(newMap.has(s[right]) && newMap.get(s[right]) >= left){
+            left = newMap.get(s[right]) + 1;
         }
-
-        objMap.set(s[right], right);
-        maxLength = Math.max(maxLength, (right - left+1));
+        newMap.set(s[right], right);
+        maxLength = Math.max(maxLength, right -left + 1);
         right++;
     }
     return maxLength;

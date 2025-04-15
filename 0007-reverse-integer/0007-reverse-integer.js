3d294ac534329600c7ee3c9cc756955274be6a74
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-04-15 10:03:43 +0600

    Time: 46 ms (78.18%), Space: 54.5 MB (81.82%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -5,19 +5,17 @@
 var reverse = function(x) {
     let reversed = 0;
     let originalNum = Math.abs(x);
-    let num = originalNum;
-    while(num > 0){
-        let digit = num % 10;
-        reversed = (reversed * 10) + digit; 
-        num = Math.floor(num / 10);
+    while(originalNum > 0){
+        let digit = originalNum % 10;
+        reversed = (reversed * 10) + digit;
+        originalNum = Math.floor(originalNum / 10);
     }
     if(x < 0){
         reversed = -reversed;
     }
-
     if(reversed < Math.pow(-2, 31) || reversed > Math.pow(2, 31) - 1){
         return 0;
     }
-
     return reversed;
 };
+

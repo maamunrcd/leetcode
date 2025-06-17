5eb54a8aa7d894291da856b91d33498ab09a49d5
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-06-17 14:10:24 +0600

    Time: 0 ms (100%), Space: 61.1 MB (52.6%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,5 +3,16 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    return s.reverse();
+    if(s.length <=1) return s;
+     let l, r;
+     l =0;
+     r = s.length-1;
+     while(l <= r){
+        let dump = s[l];
+        s[l] = s[r]
+        s[r] = dump;
+        l++;
+        r--;
+     }
+     return s;
 };

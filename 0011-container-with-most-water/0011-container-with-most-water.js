56afe395d6046c72f2b37357945d142094b8701d
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-06-18 10:59:44 +0600

    Time: 2 ms (81.48%), Space: 61.9 MB (74.68%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,18 +3,20 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let left = 0;
-    let right = height.length -1;
-    let maxFrequency = -Infinity;
+    let left, right, maxFriquency;
+    left = 0;
+    right = height.length - 1;
+    maxFriquency = -Infinity;
     while(left < right){
         let distance = right - left;
         let minContainer = Math.min(height[left], height[right]);
-        maxFrequency = Math.max(maxFrequency, minContainer*distance);
+        maxFriquency = Math.max(maxFriquency, minContainer*distance);
         if(height[left] < height[right]){
-            left++;
+            left++
         } else {
             right--;
         }
     }
-    return maxFrequency;
+    return maxFriquency;
+
 };

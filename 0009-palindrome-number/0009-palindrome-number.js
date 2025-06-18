ff694ec52d6895c272530c64ca4ffcc54f1eb600
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-06-18 10:22:31 +0600

    Time: 3 ms (98.58%), Space: 62.9 MB (97.39%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -4,11 +4,11 @@
  */
 var isPalindrome = function(x) {
     let dump = x;
-    let reversedNum = 0;
+    let reversedNumber = 0;
     while(x > 0){
         let digit = x % 10;
-        reversedNum = (reversedNum * 10) + digit;
+        reversedNumber = (reversedNumber * 10) + digit;
         x = Math.floor(x / 10);
     }
-    return dump === reversedNum;
+    return reversedNumber === dump;
 };

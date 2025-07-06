74df30b4e30c03e45d065f1cd3dafd5f8ad1982f
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-07-06 14:17:00 +0600

    Time: 68 ms (89.51%), Space: 74.3 MB (75.99%) - LeetHub

--- 2759-flatten-deeply-nested-array/2759-flatten-deeply-nested-array.js
+++ 2759-flatten-deeply-nested-array/2759-flatten-deeply-nested-array.js
@@ -5,9 +5,9 @@
  */
 var flat = function (arr, n) {
     if(n === 0) return arr;
-    let result = [];
+    const result = [];
     function flatten(arr, lavel){
-        for(item of arr){
+        for(let item of arr){
             if(Array.isArray(item) && lavel > 0){
                 flatten(item, lavel-1);
             } else {

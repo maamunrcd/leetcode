3b6620b1c8b02cc1f334a03fa68e20ba75698047
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-07-20 07:12:01 +0600

    Time: 1 ms (99.8%), Space: 61.8 MB (33.82%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,18 +11,17 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
+    let dummyNode = new ListNode();
+    let current = dummyNode;
     let curry = 0;
-    let dummyHead = new ListNode();
-    current = dummyHead;
     while(l1 || l2 || curry){
         let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + curry;
 
-        curry = Math.floor(sum / 10);
         current.next = new ListNode(sum % 10);
+        curry = Math.floor(sum / 10);
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
         current = current.next;
-
-        l1 = l1 ? l1.next:null;
-        l2 = l2 ? l2.next:null;
     }
-    return dummyHead.next;
+    return dummyNode.next;
 };

87f22da697346594f99669661f7210f4f5a2abb0
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-07-23 15:01:04 +0600

    Time: 4 ms (89.91%), Space: 55.3 MB (98.22%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,17 +2,50 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    const newMap = new Map();
-    let left, right, maxLength;
-    left = 0, right = 0, maxLength = 0;
-    while(right < s.length){
-        if(newMap.has(s[right]) && newMap.get(s[right]) >= left){
-            left = newMap.get(s[right]) + 1;
+
+//Brute Force Approach n3
+// var lengthOfLongestSubstring = function(s) {
+//     let maxLength = 0;
+//     for(let i = 0; i < s.length; i++){
+//         let seen = new Set();
+//         for(let j = i; j < s.length; j++){
+//             if(seen.has(s[j])){
+//                 break;
+//             }
+//             seen.add(s[j]);
+//             maxLength = Math.max(maxLength, j - i + 1);
+//         }
+//     }
+//     return maxLength;
+// };
+
+// Optimal Appraoch 1 - 2n
+// var lengthOfLongestSubstring = function(s){
+//     let left = 0;
+//     let maxLength = 0;
+//     let seen = new Set();
+//     for(let right = 0; right < s.length; right++){
+//         while(seen.has(s[right])){
+//             seen.delete(s[left]);
+//             left++;
+//         }
+//         seen.add(s[right]);
+//         maxLength = Math.max(maxLength, right-left+1);
+//     }
+//     return maxLength;
+// }
+
+// Optimal Appraoch 1 - 2n
+var lengthOfLongestSubstring = function(s){
+    let left = 0;
+    let maxLength = 0;
+    let map = new Map();
+    for(let right = 0; right < s.length; right++){
+        if(map.has(s[right]) && map.get(s[right]) >= left ){
+            left = map.get(s[right]) + 1;
         }
-        newMap.set(s[right], right);
-        maxLength = Math.max(maxLength, (right - left + 1));
-        right++;
+        map.set(s[right], right);
+        maxLength = Math.max(maxLength, right-left+1);
     }
     return maxLength;
-};
+}

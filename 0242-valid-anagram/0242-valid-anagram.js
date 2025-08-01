0c1e30d94d82457fd0dd80bda9d7d2a52087da1a
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-08-01 18:38:08 +0600

    Time: 12 ms (72.54%), Space: 56.9 MB (74.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,10 +7,16 @@ var isAnagram = function(s, t) {
     if(s.length !== t.length){
         return false;
     }
-    const obj = {};
+    const obj1 = {};
+    const obj2 = {};
     for(let i = 0; i < s.length; i++){
-        obj[s[i]] = (obj[s[i]] || 0) + 1;
-        obj[t[i]] = (obj[t[i]] || 0) - 1;
+        obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+        obj2[t[i]] = (obj2[t[i]] || 0) + 1;
     }
-    return Object.values(obj).every(value => value ===0);
+    for(let key in obj1){
+        if(obj1[key] !== obj2[key]){
+            return false;
+        }
+    }
+    return true;
 };

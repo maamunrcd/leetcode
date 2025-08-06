744f93c46ca1bf7d4adcd311dd6aa1affd90e9af
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-08-06 10:27:35 +0600

    Time: 33 ms (57.54%), Space: 70.3 MB (7.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,13 +3,17 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    const obj = {};
-    for(let str of strs){
-        const sortedStr = str.split("").sort().join("");
-        if(!obj[sortedStr]){
-            obj[sortedStr] = [];
+    const res = {};
+    for(let s of strs){
+        const count = new Array(26).fill(0);
+        for(let c of s){
+            count[c.charCodeAt(0) - 'a'.charCodeAt(0)] +=1;
         }
-        obj[sortedStr].push(str);
+        let key = count.join(",");
+        if(!res[key]){
+            res[key] = [];
+        }
+        res[key].push(s);
     }
-    return Object.values(obj);
+    return Object.values(res);
 };

8cfa658e3e7efdcb688a83e824d2b747978b65ac
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-08-12 09:06:23 +0600

    Time: 1 ms (87.98%), Space: 55.2 MB (41.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,9 +6,9 @@
 var twoSum = function(nums, target) {
     const obj = new Map();
     for(let i = 0; i < nums.length; i++){
-        let compare = target - nums[i];
-        if(obj.has(compare)){
-            return [i, obj.get(compare)];
+        const expect = target - nums[i];
+        if(obj.has(expect)){
+            return [i, obj.get(expect)];
         }
         obj.set(nums[i], i);
     }

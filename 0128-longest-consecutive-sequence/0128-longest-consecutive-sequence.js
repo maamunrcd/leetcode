a9b949a9f5fa22303fbadd7181e2611c958c5e71
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-08-27 06:47:03 +0600

    Time: 44 ms (32.9%), Space: 83.2 MB (16.65%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,23 +3,25 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    if (nums.length === 0) return 0;
     let maxCount = 0;
-    let map = new Map();
+    let numsMap = new Map();
+
     for(let key of nums){
-        map.set(key, true);
+        numsMap.set(key, true);
     }
-    for(let key of map.keys()){
-        if(!map.has(key-1)){
+
+    for(let key of numsMap.keys()){
+        if(!numsMap.has(key - 1)){
             let count = 1;
             let current = key;
-            while(map.has(current+1)){
+
+            while(numsMap.has(current+1)){
                 count++;
                 current++;
             }
             maxCount = Math.max(maxCount, count);
         }
-        
     }
+
     return maxCount;
 };

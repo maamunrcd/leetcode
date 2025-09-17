5ea0025b6894519d8a44fe7bd18d4854836d53d6
Md. Mamun Or Rashid <maamunrcd@gmail.com>
2025-09-17 09:18:56 +0600

    Time: 11 ms (67.29%), Space: 57.1 MB (93.82%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -5,13 +5,10 @@
  */
 var topKFrequent = function(nums, k) {
     const count = {};
-    for(const num of nums){
+    for(let num of nums){
         count[num] = (count[num] || 0) + 1;
     }
-    const arr =  Object.entries(count).map(([num, freq])=> [
-        freq,
-        parseInt(num)
-    ]);
+    let arr = Object.entries(count).map(([num, freq])=> [freq, Number(num)]);
     arr.sort((a, b)=> b[0] - a[0]);
     return arr.slice(0, k).map((pair)=> pair[1]);
 };
